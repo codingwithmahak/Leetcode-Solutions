378c058381948b8bb8cccaa8ba9c7658f056862e
codingwithmahak <codingwithmahak@gmail.com>
2025-06-06 21:39:05 +0530

    Time: 50 ms (63.86%), Space: 35 MB (36.63%) - LeetHub

--- 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
+++ 2520-using-a-robot-to-print-the-lexicographically-smallest-string/2520-using-a-robot-to-print-the-lexicographically-smallest-string.cpp
@@ -10,7 +10,6 @@ public:
         std::vector<char> minSuffix(n);
         minSuffix[n - 1] = s[n - 1];
 
-        // Step 1: Build minSuffix array
         for (int i = n - 2; i >= 0; --i) {
             minSuffix[i] = std::min(s[i], minSuffix[i + 1]);
         }
@@ -22,7 +21,6 @@ public:
         while (index < n) {
             t.push(s[index]);
 
-            // While we can pop from stack to form smaller lex result
             while (!t.empty() && (index == n - 1 || t.top() <= minSuffix[index + 1])) {
                 result += t.top();
                 t.pop();
@@ -31,7 +29,6 @@ public:
             index++;
         }
 
-        // Pop any remaining elements from the stack
         while (!t.empty()) {
             result += t.top();
             t.pop();
