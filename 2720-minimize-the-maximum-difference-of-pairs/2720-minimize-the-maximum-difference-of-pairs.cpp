7d97c73bfa48a69ad6d2a01405e8819d917cd7c3
codingwithmahak <codingwithmahak@gmail.com>
2025-06-13 20:46:04 +0530

    Time: 24 ms (68.64%), Space: 86.9 MB (6.58%) - LeetHub

---
+++ 2720-minimize-the-maximum-difference-of-pairs/2720-minimize-the-maximum-difference-of-pairs.cpp
@@ -0,0 +1,34 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int minimizeMax(vector<int>& nums, int p) {
+        sort(nums.begin(), nums.end());
+        int left = 0, right = nums.back() - nums.front();
+        
+        auto canFormPairs = [&](int maxDiff) {
+            int count = 0;
+            for (int i = 1; i < nums.size(); i++) {
+                if (nums[i] - nums[i - 1] <= maxDiff) {
+                    count++;
+                    i++; // Skip the next index as it's already paired
+                }
+            }
+            return count >= p;
+        };
+        
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+            if (canFormPairs(mid)) {
+                right = mid; // Try for a smaller maximum difference
+            } else {
+                left = mid + 1; // Increase the maximum difference
+            }
+        }
+        
+        return left;
+    }
+};
