181ff899a3a4ac2a73447c2f19b2fdfa4048b399
codingwithmahak <codingwithmahak@gmail.com>
2025-07-01 21:18:00 +0530

    Time: 0 ms (100%), Space: 9 MB (29.69%) - LeetHub

--- 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
+++ 3617-find-the-original-typed-string-i/3617-find-the-original-typed-string-i.cpp
@@ -1,20 +1,18 @@
 class Solution {
 public:
     int possibleStringCount(string word) {
-        int total = 1; // Start with the original string itself
+        int total = 1; 
         int n = word.size();
         
         for (int i = 0; i < n; ) {
             char currentChar = word[i];
             int count = 0;
-            
-            // Count the length of the contiguous run of the same character
             while (i < n && word[i] == currentChar) {
                 count++;
                 i++;
             }
             
-            // For a run of length count, we can have (count - 1) possibilities
+        
             total += (count - 1);
         }
         
